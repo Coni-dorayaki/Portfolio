7c19d349b1df7bd9e30aad7fc638f188dd43c025
Coni-dorayaki <small.fountain220830@gmail.com>
2020-10-05 02:56:24 +0000

    koizumi_11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,107 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('title', 'IZANAGI')
 
-        <title>IZANAGI</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 90px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 24px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            
-            .p{
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 24px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase; 
-            }
-
-            .m-b-md {
-                margin-bottom: 100px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ action('MypageController@index') }}">マイページ</a>
-                    @else
-                        <a href="{{ route('login') }}">ログイン</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">アカウント作成</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    IZANAGI
-                </div>
-
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto mainblock">
+                
+                <h1 class="maintitle">IZANAGI</h1>
+                    
                 <div class="links">
-                    <a href="{{ action('Admin\RequestController@Overwork') }}">残業申請</a>
-                    <a href="{{ action('Admin\RequestController@Holiday') }}">休暇申請</a>
-                    <a href="{{ action('MypageController@report') }}">週報提出</a>
-                    <a href="{{ action('Admin\ContactController@Inquiry') }}">情報展開</a>
-                    <a href="{{ action('Admin\ManageController@Key') }}">事務所鍵</a>
+                    <a href="{{ action('Admin\RequestController@Overwork') }}" role="button" class="btn btn-primary titlebtn">残業申請</a>
+                    <a href="{{ action('Admin\RequestController@Holiday') }}" role="button" class="btn btn-primary titlebtn">休暇申請</a>
+                    <a href="{{ action('MypageController@report') }}" role="button" class="btn btn-primary titlebtn">週報提出</a>
+                    <a href="{{ action('Admin\ContactController@Inquiry') }}" role="button" class="btn btn-primary titlebtn">情報展開</a>
+                    <a href="{{ action('Admin\ManageController@Key') }}" role="button" class="btn btn-primary titlebtn">事務所鍵</a>
                 </div>
+                    
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
